680390fe6470880e955675d3605debc9813569dc
Harpreet Singh <harry.singh1064@gmail.com>
2018-03-23 16:40:34 +0530

    Add bootstrap form component for search box.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Movie Master</div>
-        <div>
-          <input type="text" placeholder="Search movie" />
-          <button>Search</button>
-        </div>
+        <div className="App-title">Movie Master</div>
+        <FormGroup className="App-search-form">
+          <InputGroup>
+            <FormControl placeholder="Search movie" />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search" />
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
         <div>Gallery</div>
       </div>
     );
