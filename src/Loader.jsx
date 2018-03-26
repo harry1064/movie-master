0c347c0747f09a7a81cbb12f2d249e60ea738c7b
Harpreet Singh <harry.singh1064@gmail.com>
2018-03-26 15:42:47 +0530

    Fix class to className in jsx.

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Loader extends Component {
   render() {
     return (
-      <div class="lds-ripple">
+      <div className="lds-ripple">
         <div />
         <div />
       </div>
